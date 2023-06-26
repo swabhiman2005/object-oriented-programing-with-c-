189b1abddcbee7590d84062e8f40d3490d9ff408
swabhiman raj <123272965+swabhiman2005@users.noreply.github.com>
2023-06-26 21:00:16 +0530

    demonstrates the use of relational operators

---
+++ write a c++ program that demonstrates the use of relational operators.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+	int x=10,y=20;
+	cout<<"\n"<<x<<"<"<<y<<"="<<(x<y);
+	cout<<"\n"<<x<<"=="<<y<<"="<<(x==y);
+	cout<<"\n"<<x<<"!"<<y<<"="<<(x!=y);
+	cout<<"\n"<<x<<">"<<y<<"="<<(x>y);
+	cout<<"\n"<<x<<">="<<y<<"="<<(x>=y);
+	cout<<"\n"<<x<<"<="<<y<<"="<<(x<=y);
+	return 0;
+}
